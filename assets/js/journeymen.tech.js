a6a8434b976735cba57b3cc010643301c5c9a69b
Vaibhav Joshi <joshi93vaibhav@gmail.com>
2021-12-14 23:37:19 +0530

    First rendition

---
+++ assets/js/journeymen.tech.js
@@ -0,0 +1,21 @@
+window.onscroll = function () { scrollFunction() };
+
+function vh() {
+    return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+}
+
+function scrollFunction() {
+    if (document.documentElement.clientWidth < 992 || window.innerWidth < 992) return
+    const navbar = document.getElementById("navbar")
+    if (document.body.scrollTop > vh() || document.documentElement.scrollTop > vh())
+        navbar.classList.remove('bg-transparent')
+    else
+        navbar.classList.add('bg-transparent')
+}
+
+(function () {
+    const navbar = document.getElementById('navbar')
+    if (document.documentElement.clientWidth >= 992 || window.innerWidth >= 992)
+        navbar.classList.add('bg-transparent')
+    // toggle navbar on smaller screens if a link is clicked.
+})()
